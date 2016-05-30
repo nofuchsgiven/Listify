71ab5cf86b070054b0fc234b1bf1ce5e3ff75cd2
Maximilian Pelzer <maxpelzer@pat1.sdi.h-da.de>
2016-05-30 17:10:14 +0200

    Initial Commit

---
+++ listify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  listify
+//
+//  Created by Max on 30.05.16.
+//  Copyright © 2016 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
